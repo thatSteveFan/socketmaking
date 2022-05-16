fa5243e75e47b7fc7e70c06466c43320c2e2d72b
Pramukh Naduthota <pnaduthota@google.com>
2022-05-16 22:28:57 +0000

    first

---
+++ mk_serv_socket.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+
+import socket
+from socket import *
+import time
+
+HOST = ""
+PORT = 5000
+def main():
+    s = socket()
+    s.setsockopt(IPPROTO_TCP, TCP_MAXSEG, 8000)
+    s.connect((HOST, PORT))
+    print('Connected to ', HOST)
+    r = b'a'
+    print(f"sending {r}")
+    s.send(r)
+    time.sleep(60)
+
+
+
+if __name__ == '__main__':
+    main()
